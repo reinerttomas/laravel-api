f059f9deb3b01a0c65dc3666a8beb30548f0f608
Tomáš Reinert <reinerttomas@gmail.com>
2023-04-12 07:17:24 +0200

    fix

--- database/migrations/2023_04_10_185018_create_customers_table.php
+++ database/migrations/2023_04_10_185018_create_customers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('type'); // Individual or Business
+            $table->integer('type'); // Individual or Business
             $table->string('email');
             $table->string('address');
             $table->string('city');
